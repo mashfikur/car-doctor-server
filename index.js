fb2576558ad573ac1a4b08bd2dec1ac59f54c3b8
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-29 02:41:35 +0600

    indiviudal booking id deletetion process enabled

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/bookings/:id/delete", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
