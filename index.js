c350ee66085348fd48159548f48b0d23c6411688
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-29 17:19:53 +0600

    jwt token and cookie exchange method implemented

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser")
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -7,14 +9,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }));
+
 app.use(express.json());
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Welcome to car doctor server");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnoho8k.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri = "mongodb://localhost:27017"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,6 +47,21 @@ async function run() {
       .db("carDoctorDB")
       .collection("bookingsCollection");
 
+    // authentication token related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // main application related api
     app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
@@ -56,7 +80,7 @@ async function run() {
       const query = { userId: userId };
       const cursor = bookingsCollection.find(query);
       const result = await cursor.toArray();
-
+      console.log("token",req.cookies.token)
       res.send(result);
     });
 
@@ -70,7 +94,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await bookingsCollection.deleteOne(query);
-
       res.send(result);
     });
 
